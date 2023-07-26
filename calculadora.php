312e0cd024f32315b88e5afb89512feb10a187c5
Gregorylevete <137961768+Gregorylevete@users.noreply.github.com>
2023-07-25 22:07:12 -0300

    Update calculadora.php

--- calculadora.php
+++ calculadora.php
@@ -1,29 +1,49 @@
 <?php
 
-// + - * / 
-$value ='24';
-$value2 = "24";
-$operador  = "+";
+// + - *, /, ^, %, 
+$value = 2;
+$value2 = 2;
+$operador  = "%";
 
 $resultado = 0;
 
+// si el operador es division
 if ($operador == "/") {
     $resultado = $value / $value2; 
 }
 
+// si el operador es multiplicado
 if ($operador == "*") {
     $resultado = $value * $value2; 
 }
 
 
+// si el operador es restado
 if ($operador == "-") {
     $resultado = $value - $value2; 
 }
 
-
+// si el operador es sumado
 if ($operador == "+") {
     $resultado = $value + $value2; 
 }
 
-echo $resultado;
+// si el operador es potenciado
+if ($operador == "pow") {
+    // mostrara el resulta elevado a potenciado
+    $resultado = pow($value, $value2); 
+}
+// si el operador 
+if ($operador == "%") {
+    // mostrara el resultado 
+    $resto =  ($value % $value2); 
+    if ($resto == 0) {
+        $resultado = "es par";
+    }else{
+        $resultado = "no es par";
+
+    }
+}
+
+echo "O valor é: {$resultado}";
 
